6417da530253dc638334f8c8958807ded7030e1a
Gnanes99 <gnaneswar.ramki@gmail.com>
2026-08-20 18:21:45 +0800

    Renamed chatbot from Duke to Dennis and updated banner

---
+++ src/main/java/Dennis.java
@@ -0,0 +1,11 @@
+// reused existing code, changed banner and filename
+public class Dennis {
+    public static void main(String[] args) {
+        String banner = " ____                   _     \n"
+                + "|  _ \\  ___ _ __  _ __ (_)___ \n"
+                + "| | | |/ _ \\ '_ \\| '_ \\| / __|\n"
+                + "| |_| |  __/ | | | | | | \\__ \\\n"
+                + "|____/ \\___|_| |_|_| |_|_|___/\n";
+        System.out.println(banner);
+    }
+}
