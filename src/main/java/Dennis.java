fe2830af344df8919052a5975e8e367628c54938
Gnanes99 <gnaneswar.ramki@gmail.com>
2026-08-20 21:49:20 +0800

    Updated Dennis to greet user and exit

--- src/main/java/Dennis.java
+++ src/main/java/Dennis.java
@@ -7,5 +7,10 @@ public class Dennis {
                 + "| |_| |  __/ | | | | | | \\__ \\\n"
                 + "|____/ \\___|_| |_|_| |_|_|___/\n";
         System.out.println(banner);
+        System.out.println("Hi, my name is Dennis. It is lovely to meet you!");
+        System.out.println("How may I help you today?");
+        System.out.println("_____________________________________________________");
+        System.out.println("Bye. Looking forward to seeing you again!");
+
     }
 }
